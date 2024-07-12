eabae1802ba2d3dda83195d18361c9c1f9152071
Bruno Batista <101836744+batista92@users.noreply.github.com>
2024-07-12 03:17:50 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Não! Cê é noob cara, que locura. Que coisa absurda!</h1>
+    return <h1>Não! Cê é noob cara, que locura. Que coisa absurda!!!</h1>
 }
 
 export default Home;
